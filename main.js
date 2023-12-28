b0431425e3bdab0eb54206d95faf82a251df07e1
unknown <paulpleela@gmail.com>
2023-12-29 01:48:12 +0700

    Added animations

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import './style.css'
+
+const observer_x = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        entry.target.classList.toggle('inside-view-x', entry.isIntersecting);
+    });
+});
+
+const sections_x = document.querySelectorAll(".outside-view-x");
+sections_x.forEach((elem) => observer_x.observe(elem));
+
+
+const observer_y = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        entry.target.classList.toggle('inside-view-y', entry.isIntersecting);
+    });
+});
+
+const sections_y = document.querySelectorAll(".outside-view-y");
+sections_y.forEach((elem) => observer_y.observe(elem));
